bc19a5b28bbf0be25aa21843ab77853f7b2b02b7
Rubens Soares <rubens.soares@sensedia.com>
2022-01-16 23:10:40 -0300

    Created Repository, service and configured H2 database.

--- src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -6,6 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -13,8 +18,12 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
